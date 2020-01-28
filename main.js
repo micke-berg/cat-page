8c76d29857e036d7e65ce599124a78670eb2358a
Micke Berg <micke.berg.dev@gmail.com>
2020-01-28 15:21:59 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,76 @@
+const btnNone = document.querySelector('#btn-no-sort');
+const btnMuchCute = document.querySelector('#btn-much-cute');
+const btnNotCute = document.querySelector('#btn-not-cute');
+const imageContainer = document.querySelector('#image-container');
+
+
+let url = "http://localhost:8081/catdata.json";
+let catsArr = [];
+let catsOriginalArray = [];
+
+let head = new Headers();
+    head.append("Accept", "application/json");
+
+let req = new Request(url, {
+  method: "GET", 
+  headers: head,
+  cache: 'no-cache',
+  mode: "cors",
+});
+
+fetch(req)
+.then((resp) => {
+  if(resp.ok) {
+    return resp.json();
+  } 
+  else{
+    throw new Error("Nope... you´ve got a problem here!");
+  }
+})
+.then((data) => {
+  console.log(data);
+    catsArr = data.cats;
+    catsOriginalArray = [...catsArr];
+    printCats();
+})
+.catch((err) => {
+  console.log(err);
+  console.log("Error:", err.message);
+});
+
+function notSorted() {
+  catsArr = [...catsOriginalArray];
+  printCats();
+}
+
+function muchCute() {
+  catsArr.sort((a, b) => {
+    return b.cutenessLevel - a.cutenessLevel;
+  })
+  printCats();
+}
+
+function notCute() {
+  catsArr.sort((a, b) => {
+    return a.cutenessLevel - b.cutenessLevel;
+    console.log(a.cutenessLevel + 'Sooo cute!');
+  })
+  printCats();
+}
+
+function printCats() {
+  imageContainer.innerHTML = '';
+  catsArr.map(cat => {
+    imageContainer.innerHTML += `
+    <div class="card">
+      <div class="image">
+        <img src="./images/${cat.image}" alt="cat">
+      </div>
+      <p class="name">${cat.name}</p>
+    </div>`
+  })
+}
+
+btnNone.addEventListener('click', notSorted);
+btnMuchCute.addEventListener('click', muchCute);
+btnNotCute.addEventListener('click', notCute);
